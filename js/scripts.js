a6e9c717b3672201bb0bea8e07a9666785ea3f67
simon-lefebvre <74560487+simon-lefebvre@users.noreply.github.com>
2020-11-17 12:03:13 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
-/*!
-    * Start Bootstrap - Grayscale v6.0.2 (https://startbootstrap.com/themes/grayscale)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
-    */
+
     (function ($) {
     "use strict"; // Start of use strict
 
